a5644fb99733d1c16fef63fa8a2dd832768920e7
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-05-30 23:48:05 +0600

    project init and different dashboards setup

---
+++ django-project/task-management/tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from tasks.views import manager_dashboard, user_dashboard
+
+urlpatterns = [
+    path('manager_dashboard/', manager_dashboard),
+    path('user_dashboard/', user_dashboard),
+]
