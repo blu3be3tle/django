1bac3f0144ce68ccf2253b7d07d444d16aca5c12
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-06-11 22:55:24 +0600

    practice3: create django form

--- django-project/task-management/tasks/urls.py
+++ django-project/task-management/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import manager_dashboard, user_dashboard
+from tasks.views import manager_dashboard, user_dashboard, create_task
 
 urlpatterns = [
     path('manager_dashboard/', manager_dashboard),
     path('user_dashboard/', user_dashboard),
+    path('create_task/', create_task)
 ]
--- django-project/task-management/tasks/views.py
+++ django-project/task-management/tasks/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from tasks.forms import TaskForm
 
 # Create your views here.
+
 
 def manager_dashboard(request):
     return render(request, "dashboard/manager-dashboard.html")
 
+
 def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
+
+
+def create_task(request):
+    form = TaskForm()
+    context = {"form": form}
+    return render(request, "task_form.html", context)
