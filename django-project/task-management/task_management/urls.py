c66b395d0247d9a0e7565cb2f6c7068eb7ebf7c5
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-06-19 22:59:17 +0600

    django debug toolbar setup

--- django-project/task-management/task_management/urls.py
+++ django-project/task-management/task_management/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls'))
-] + debug_toolbar_urls()
+]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
