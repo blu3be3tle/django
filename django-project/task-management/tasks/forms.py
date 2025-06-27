e34b9bd9342b19114aca153971eb59b77a44ccbf
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-06-27 20:36:09 +0600

    practice4: django orm

--- django-project/task-management/tasks/forms.py
+++ django-project/task-management/tasks/forms.py
@@ -1,14 +1,32 @@
 from django import forms
-from django.contrib.auth.models import User
 from tasks.models import Task
+
+# Django Form
+
+
+class TaskForm(forms.Form):
+    title = forms.CharField(max_length=250, label="Task Title")
+    description = forms.CharField(
+        widget=forms.Textarea, label='Task Description')
+    due_date = forms.DateField(widget=forms.SelectDateWidget, label="Due Date")
+    assigned_to = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple, choices=[], label='Assigned To')
+
+    def __init__(self, *args, **kwargs):
+        # print(args, kwargs)
+        employees = kwargs.pop("employees", [])
+        super().__init__(*args, **kwargs)
+        self.fields['assigned_to'].choices = [
+            (emp.id, emp.name) for emp in employees]
 
 
 class StyledFormMixin:
-    """Mixin to apply Tailwind-style classes to form fields."""
+    """ Mixing to apply style to form field"""
+
     default_classes = "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
 
     def apply_styled_widgets(self):
-        for field_name, field in self.fields.items():
+        for field_name, field in self.fields.items(): # type: ignore
             if isinstance(field.widget, forms.TextInput):
                 field.widget.attrs.update({
                     'class': self.default_classes,
@@ -17,39 +35,27 @@ class StyledFormMixin:
             elif isinstance(field.widget, forms.Textarea):
                 field.widget.attrs.update({
                     'class': f"{self.default_classes} resize-none",
-                    'placeholder': f"Enter {field.label.lower()}",
+                    'placeholder':  f"Enter {field.label.lower()}",
                     'rows': 5
                 })
             elif isinstance(field.widget, forms.SelectDateWidget):
+                print("Inside Date")
                 field.widget.attrs.update({
-                    "class": self.default_classes
+                    "class": "border-2 border-gray-300 p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
                 })
             elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                print("Inside checkbox")
                 field.widget.attrs.update({
                     'class': "space-y-2"
                 })
             else:
+                print("Inside else")
                 field.widget.attrs.update({
                     'class': self.default_classes
                 })
 
 
-class TaskForm(forms.Form):
-    title = forms.CharField(max_length=250, label="Task Title")
-    description = forms.CharField(
-        widget=forms.Textarea, label="Task Description")
-    due_date = forms.DateField(widget=forms.SelectDateWidget, label="Due Date")
-    assigned_to = forms.MultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple, choices=[], label="Assigned To"
-    )
-
-    def __init__(self, *args, **kwargs):
-        employees = kwargs.pop("employees", [])
-        super().__init__(*args, **kwargs)
-        self.fields['assigned_to'].choices = [
-            # Assuming you're using User model
-            (emp.id, emp.username) for emp in employees
-        ]
+# Django Model Form
 
 
 class TaskModelForm(StyledFormMixin, forms.ModelForm):
@@ -61,6 +67,8 @@ class TaskModelForm(StyledFormMixin, forms.ModelForm):
             'assigned_to': forms.CheckboxSelectMultiple
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """ Widget using mixins """
+
+    def __init__(self, *arg, **kwarg):
+        super().__init__(*arg, **kwarg)
         self.apply_styled_widgets()
