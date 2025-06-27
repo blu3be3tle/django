8f7112d7f9ba145f0d69cb8c76fc51c19cd68790
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-06-27 22:03:04 +0600

    practice5: links updated

--- django-project/task-management/tasks/urls.py
+++ django-project/task-management/tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from tasks.views import manager_dashboard, user_dashboard, create_task, view_task
 
 urlpatterns = [
-    path('manager-dashboard/', manager_dashboard),
-    path('user-dashboard/', user_dashboard),
-    path('create-task/', create_task),
+    path('manager_dashboard/', manager_dashboard),
+    path('user_dashboard/', user_dashboard),
+    path('create_task/', create_task),
     path('view_task/', view_task)
 ]
