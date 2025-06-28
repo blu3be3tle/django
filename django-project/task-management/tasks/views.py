664eb704aad2ba7ad836be50fb2bc70ad71caa9f
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-06-28 16:01:27 +0600

    practice5: created dynamic dashboard

--- django-project/task-management/tasks/views.py
+++ django-project/task-management/tasks/views.py
@@ -10,24 +10,27 @@ from django.db.models import Q, Count, Max, Min, Avg
 
 
 def manager_dashboard(request):
-    return render(request, "dashboard/manager-dashboard.html")
+    tasks = Task.objects.all()
+
+    # tasks
+    total_task = tasks.count()
+    completed_task = Task.objects.filter(status='COMPLETED').count()
+    in_progress_task = Task.objects.filter(status='IN_PROGRESS').count()
+    pending_task = Task.objects.filter(status='PENDING').count()
+
+    context = {
+        'tasks': tasks,
+        'total_task': total_task,
+        'completed_task': completed_task,
+        'in_progress_task': in_progress_task,
+        'pending_task': pending_task
+    }
+
+    return render(request, "dashboard/manager-dashboard.html", context)
 
 
 def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
-
-
-def test(request):
-    names = ["Mahmud", "Ahamed", "John", "Mr. X"]
-    count = 0
-    for name in names:
-        count += 1
-    context = {
-        "names": names,
-        "age": 23,
-        "count": count
-    }
-    return render(request, 'test.html', context)
 
 
 def create_task(request):
